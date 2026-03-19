a26db295d0d696dc226ae0cfbf60c2e5802a12fc
alexandrerussi <alexandrerussijunior@gmail.com>
2026-03-19 11:42:26 -0300

    aula 02 variaveis

---
+++ aula02-variaveis-operadores/aula02a-variaveis.py
@@ -0,0 +1,38 @@
+print("ola mundo")
+
+print(7 + 4)
+print("7 + 4")
+print("7" + "4") # CONCATENANDO STRINGS
+
+# Comentário de 1 linha
+'''
+    Comentários de
+    mútliplas
+    linhas
+'''
+
+# VARIÁVEIS
+nome = "Alexandre" # str
+idade = 26 # int
+peso = 70.2 # float
+
+print("oiii \n", nome, idade, peso)
+print(f"Olá, {nome}!!!")
+
+# INPUT -- SIMULAÇÃO DE UM FORMS NO CMD
+nome = input("Digite o seu nome: ")
+idade = int(input("Digite sua idade: "))
+peso = float(input("Digite seu peso: "))
+
+print(nome, idade, peso)
+print(idade + 1)
+
+ano_nascimento = 1999
+ano_atual = 2026
+idade = ano_atual - ano_nascimento
+print(f"Sua idade é: {idade}")
+
+
+
+
+
